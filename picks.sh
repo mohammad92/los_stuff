45c5388712ce91d6b422f5d08ad583676700fd9e
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2019-11-17 23:45:05 +0200

    Updated picks

--- picks.sh
+++ picks.sh
@@ -23,22 +23,15 @@ repopick -c 10 -t fix_signal
 repopick -c 10 -t ten-ims
 repopick -c 20 -t ten-qc-telephony-fix
 repopick -c 30 -t ten-qc-ims # And frameworks/opt/net/ims 
-# repopick 262175 # GsmCdmaPhone: Fix GSM SIM card ICCID on NV sub CDMA devices
 
 # hardware/samsung
-repopick 262433 # samsung: ril: update libril to latest aosp
-repopick 256236 # samsung: AdvancedDisplay: Migrate to androidx
-
-# packages/providers/TelephonyProvider
-repopick 256780 # TelephonyProvider: add upgrade support from cm-14.1
-repopick 263780 # [SQUASH] Merge android-10.0.0_r11 into lineage-17.0
+# repopick 262433 # samsung: ril: update libril to latest aosp
+# repopick 256236 # samsung: AdvancedDisplay: Migrate to androidx
 
 # packages/services/Telephony
 repopick 256792 # Telephony: Add ERI configuration for U.S. Cellular
 repopick 256793 # Telephony: Support muting by RIL command
 repopick 256795 # Don't start SIP service before decrypted
-repopick 263783 # [SQUASH] Merge android-10.0.0_r11 into lineage-17.0
-repopick -f 256797 # Allow to disable the new scan API for manual network search.
 
 # system/netd
 repopick 256959 # netd: Allow devices to force-add directly-connected routes
