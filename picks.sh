2815cf749f222eeffda648d78df2b0a8a166ef4f
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2019-11-24 19:12:53 +0200

    Update picks

--- picks.sh
+++ picks.sh
@@ -19,14 +19,13 @@ repopick 258601 # sensorservice: customize sensor fusion mag filter via prop
 repopick 259448 # libui: Allow invalid bits from callers conditionally
 
 # frameworks/opt/telephony
-repopick -c 10 -t fix_signal
 repopick -c 10 -t ten-ims
 repopick -c 20 -t ten-qc-telephony-fix
 repopick -c 30 -t ten-qc-ims # And frameworks/opt/net/ims 
 
 # hardware/samsung
 # repopick 262433 # samsung: ril: update libril to latest aosp
-# repopick 256236 # samsung: AdvancedDisplay: Migrate to androidx
+repopick 256236 # samsung: AdvancedDisplay: Migrate to androidx
 
 # packages/services/Telephony
 repopick 256792 # Telephony: Add ERI configuration for U.S. Cellular
@@ -38,3 +37,4 @@ repopick 256959 # netd: Allow devices to force-add directly-connected routes
 
 # vendor/lineage
 repopick -c 20 256960 # soong_config: add TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE
+repopick 258204 # build: set build fingerprint for all devices
